03d9e4d15160bb1f450c779103d255dc0adc1e03
Spencer Lepine <spencerlepine26@gmail.com>
2022-07-11 20:39:50 -0700

    Heroku Deployment (#2)
    * rename workflow file
    
    * use CORS
    
    * test new Heroku Deployment workflow

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 module.exports = {
   PORT: process.env.PORT || 8888,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  TOKEN_KEY: process.env.TOKEN_KEY || 'testToken',
+  JWT_TOKEN_KEY: process.env.JWT_TOKEN_KEY || 'testToken',
   API_SERVICE_URL: process.env.API_SERVICE_URL || 'http://localhost:3000'
 }
